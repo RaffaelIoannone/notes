8d4489abca6155252e20afa16c5118bd3b481065
Raffael Ioannone <raffael@Raffaels-MacBook-Pro.local>
2020-06-03 14:27:16 +0200

    Extended initial code a bit

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 import Note from './modules/note.js';
 
 const noteList = [];
-noteList.push(new Note('CAS FEE Selbstudium / Projekt', 'HTML für die note App erstellen. CSS erstellen für die note App', 2, new Date('2020-05-22')));
-noteList.push(new Note('Einkaufen', 'Milch, Brot, Salat, Steak', 5, new Date('2020-05-15')));
+noteList.push(
+  new Note('Einkaufen', 'Milch, Brot, Salat, Steak', 5, new Date('2020-05-15'))
+);
 noteList.forEach((a) => console.log(a));
+
+
+const form = document.getElementById('form-add-note');
+const title = document.getElementById('title');
+const description = document.getElementById('description');
+const importance = document.getElementById('importance');
+const duedate = document.getElementById('duedate');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  noteList.push(
+    new Note(title.value, description.value, importance.value, duedate.value)
+  );
+  noteList.forEach((a) => console.log(a));
+});
+
+const themeSelect = document.getElementById('theme-select');
+themeSelect.addEventListener('change', () => {
+  const classes = [...document.body.classList];
+  classes.filter((classname) => classname.includes('-theme'))
+    .forEach((classname) => document.body.classList.toggle(classname));
+  document.body.classList.toggle(themeSelect.options[themeSelect.selectedIndex].value);
+});
--- modules/note.js
+++ modules/note.js
@@ -1,10 +1,10 @@
 class Note {
-  constructor(title, desscription, importance, finishedUntil, finished = false) {
+  constructor(title, desscription, importance, dueDate, finishDate = null) {
     this.title = title;
     this.desscription = desscription;
     this.importance = importance;
-    this.finishedUntil = finishedUntil;
-    this.finished = finished;
+    this.dueDate = dueDate;
+    this.finishDate = finishDate;
   }
 }
 
